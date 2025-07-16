fad47f29418e2c8ef8544014da00954dd105a834
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-07-16 23:00:52 +0600

    new project done

---
+++ julysixteen/src/main/java/com/emranhss/julysixteen/JulySixteenApplication.java
@@ -0,0 +1,15 @@
+package com.emranhss.julysixteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulySixteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulySixteenApplication.class, args);
+
+		System.out.println("hello");
+	}
+
+}
---
+++ julysixteen/src/test/java/com/emranhss/julysixteen/JulySixteenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emranhss.julysixteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulySixteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
