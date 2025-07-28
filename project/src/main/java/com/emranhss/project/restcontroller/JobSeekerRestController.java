719a621357229e8d9ef470c3bcebaba2d337c409
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-07-28 16:44:08 +0600

    SaveDone but Some error

--- project/src/main/java/com/emranhss/project/restcontroller/JobSeekerRestController.java
+++ project/src/main/java/com/emranhss/project/restcontroller/JobSeekerRestController.java
@@ -5,6 +5,7 @@ import com.emranhss.project.entity.User;
 import com.emranhss.project.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ import java.util.Map;
 @RequestMapping("/api/jobseeker/")
 public class JobSeekerRestController {
 
+    @Autowired
     private UserService userService;
 
     @PostMapping("")
