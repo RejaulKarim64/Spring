345557bef2513e00f13e11ba03137f38a4a4c33d
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-08-10 19:09:59 +0600

    adddd

--- project_ng/src/app/service/user-service.ts
+++ project_ng/src/app/service/user-service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../model/user.model';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +12,23 @@ export class UserService {
 
   private baseUrl: string = environment.apiUrl + '/user/';
 
-constructor(
-  private http: HttpClient
-) { }
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
 
-public getAllEmp(): Observable<User[]> {
-  return this.http.get<User[]>(this.baseUrl);
-}
+  public getAllEmp(): Observable<User[]> {
 
-  
+    let headers = new HttpHeaders();
+
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem('authToken');
+      if (token) {
+        headers = headers.set('Authorization', 'Bearer ' + token);
+      }
+    }
+    return this.http.get<User[]>(this.baseUrl);
+  }
+
+
 }
